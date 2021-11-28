28d17e755f9be382ff5c45af44314a1c1c730f80
Akim Grakovskiy <aakim5586@gmail.com>
2021-11-28 23:15:05 +0200

    28.11.2021 23-15

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,67 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const refs = {
+    gallery: document.querySelector('.gallery'),
+    galleryItem: document.querySelector('.gallery__item'),
+    galleryLink: document.querySelector('.gallery__link'),
+    galleryImage: document.querySelector('.gallery__image'),
+    galleryModal: "",
+    imageToOpen: "",
+};
+
+
+createGalleryMarkup(galleryItems);
+function createGalleryMarkup(galleryItems) {
+    const markup = galleryItems
+        .map(item =>
+            `
+            <div class="gallery__item">
+            <a class="gallery__link"
+                href="${item.original}">
+                <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+                />
+            </a>
+            </div>
+        `
+        )
+        .join(" ");
+
+    refs.gallery.innerHTML = markup;
+};
+
+refs.gallery.addEventListener('click', openModal);
+function openModal(event) {
+
+        event.preventDefault();
+
+        if (event.target.nodeName !== 'IMG') {
+            return
+        }
+        refs.imageToOpen = event.target.dataset.source;
+        refs.galleryModal = basicLightbox.create(`
+            <div class="modal">
+                <img
+                    src= "${refs.imageToOpen}"
+                    style="width: 800px" />
+            </div>
+`);
+        refs.galleryModal.show()
+        document.body.addEventListener('keydown', closeModal);
+}
+
+    function closeModal(event) {
+        const isEscKey = event.code === 'Escape';
+        const galleryIsFilled = refs.galleryModal !== "";
+        if (isEscKey || galleryIsFilled) {
+            refs.galleryModal.close();
+            refs.imageToOpen = "";
+        }
+        document.body.removeEventListener('keydown', onEscKeyPress);
+
+    }
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+    gallery: document.querySelector('.gallery'),
+    galleryItem: document.querySelector('.gallery__item'),
+    galleryLink: document.querySelector('.gallery__link'),
+    galleryImage: document.querySelector('.gallery__image'),
+    galleryModal: "",
+    imageCaption: "",
+};
+
+createGalleryMarkup(galleryItems);
+function createGalleryMarkup(galleryItems) {
+    const markup = galleryItems
+        .map(item =>
+            `
+            <a class="gallery__item"
+                href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    alt="${item.description}"
+                />
+            </a>
+        `
+        )
+        .join(" ");
+
+    refs.gallery.innerHTML = markup;
+};
+
+refs.gallery.addEventListener('click', openModal);
+function openModal(event) {
+
+        event.preventDefault();
+
+        if (event.target.nodeName !== 'IMG') {
+            return
+        }
+
+    console.log('click on image');
+    refs.imageCaption = event.target.alt;
+    console.log(refs.imageCaption);
+    refs.galleryModal = new SimpleLightbox('.gallery a', { captionType: "${refs.imageCaption}", animationSpeed: "250" });
+};
+
+
